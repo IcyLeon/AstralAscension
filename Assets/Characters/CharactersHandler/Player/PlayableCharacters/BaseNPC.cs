b452718dd4047fa158eae33561c81521f2232912
Randall <88996884+IcyLeon@users.noreply.github.com>
2024-04-09 04:06:17 +0800

    NPC test

---
+++ Assets/Characters/CharactersHandler/Player/PlayableCharacters/BaseNPC.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseNPC : Characters
+{
+    [SerializeField] private NPCInteract NPCInteract;
+    protected override void Update()
+    {
+        base.Update();
+        UpdateInteractionTransform(NPCInteract.closestInteractionTransform);
+    }
+}
